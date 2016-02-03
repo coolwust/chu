e719d44b2863b7815690082af4989f0e475cf026
coolwust <coolwust@gmail.com>
2016-02-02 20:47:20 -0500

    fix

---
+++ public/js/base.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+//document.addEventListener('DOMContentLoaded', function () {
+//  var icon = document.querySelector('.menu-icon');
+//  var menu = document.querySelector('.nav ul');
+//  var show = false;
+//  icon.addEventListener('click', function () {
+//    if (show === false) {
+//      menu.style.display = 'table';
+//      icon.style.border = '1px solid #333';
+//      icon.style.borderBottom = 'none';
+//      icon.style.color = '#e50012';
+//      show = true;
+//    } else {
+//      menu.style.display = 'none';
+//      icon.style.border = 'none';
+//      icon.style.color = '#333';
+//      show = false;
+//    }
+//  });
+//});
